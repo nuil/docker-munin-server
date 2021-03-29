9d4ed654d35a6c0eb35f4b7d7bb60512e13143f9
Sebastian Ewald <sebastian003@googlemail.com>
2021-03-29 15:31:44 +0200

    fixed host check

--- start-munin.sh
+++ start-munin.sh
@@ -52,7 +52,7 @@ do
   if [ ${#PORT} -eq 0 ]; then
       PORT=4949
   fi
-  if ! grep -q $HOST /etc/munin/munin.conf ; then
+  if ! grep -q $NAME /etc/munin/munin.conf ; then
     cat << EOF >> /etc/munin/munin.conf
 [$NAME]
     address $HOST
@@ -72,7 +72,7 @@ do
   if [ ${#PORT} -eq 0 ]; then
       PORT=4949
   fi
-  if ! grep -q $HOST /etc/munin/munin.conf ; then
+  if ! grep -q $NAME /etc/munin/munin.conf ; then
     cat << EOF >> /etc/munin/munin.conf
 [$NAME]
     address $HOST
